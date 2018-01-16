9cab0462ee670e5a278d7a1bbdce8a086b185293
giacalonenick <35347941+giacalonenick@users.noreply.github.com>
2018-01-15 19:41:19 -0800

    Wrote Header

---
+++ Homework00/Hello World.java
@@ -0,0 +1,15 @@
+/*
+File name:helloWorld
+Purpose:Greets the world by saying "Hello, World!"
+Author:Nick Giacalone  
+Date:15 January 2018
+Description:First homework assignment
+*/
+
+
+
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
